10fc105231e32ef7221690e9c2074225065c9272
David Jiang <djiang86@binghamton.edu>
2018-10-02 02:29:20 +0000

    implementing the cloneable interface

---
+++ Prototype/Shape.java
@@ -0,0 +1,28 @@
+package Advanced.Prototype;
+
+public abstract class Shape implements Cloneable {
+    private String id;
+    private String type;
+
+    abstract void draw();
+
+    public String getType() {
+        return type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Object clone() {
+        Object clone = null;
+
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return clone;
+    }
+}

1e18d41ad82d7473718c4e3ab64f8a0a59c0ee79
David Jiang <djiang86@binghamton.edu>
2018-10-02 17:24:47 +0000

    creating criteria interface for filter design

---
+++ Filter/Criteria.java
@@ -0,0 +1,7 @@
+package Advanced.Filter;
+
+import java.util.List;
+
+public interface Criteria {
+    public List<Person> meetCriteria(List<Person> persons);
+}
---
+++ Filter/Main.java
@@ -0,0 +1,6 @@
+package Advanced.Filter;
+
+// filtering a set of objects using different criteria and chaining them in a decoupled
+// way through logical operations.
+public class Main {
+}

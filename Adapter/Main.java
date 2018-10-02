3fc22fabbbf7959901215c7fec3d8b41cdc48f3e
David Jiang <djiang86@binghamton.edu>
2018-10-02 16:44:15 +0000

    adaptor complete

--- Adapter/Main.java
+++ Adapter/Main.java
@@ -4,6 +4,11 @@ package Advanced.Adapter;
 // of 2 different or incompatible classes. Acting as a bridge between 2 incompatible interfaces
 public class Main {
     public static void main(String[] args) {
+        AudioPlayer audioPlayer = new AudioPlayer();
 
+        audioPlayer.play("mp3", "Beyond the horizon");
+        audioPlayer.play("mp4", "alone");
+        audioPlayer.play("vlc", "far far away");
+        audioPlayer.play("avi", "hello");
     }
 }

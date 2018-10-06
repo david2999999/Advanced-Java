586860024b02399b8692fd3028923be5dba05c77
David Jiang <djiang86@binghamton.edu>
2018-10-06 23:53:51 +0000

    subject and abstract observer class

---
+++ Observer/Subject.java
@@ -0,0 +1,28 @@
+package Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<>();
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        notifyAllObserver();
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObserver() {
+        for (Observer observer: observers) {
+            observer.update();
+        }
+    }
+}

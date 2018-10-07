e22fdef97b66228ef496f272f34d45de57f3fb18
David Jiang <djiang86@binghamton.edu>
2018-10-07 04:02:56 +0000

    checking for services in the cache first before looking up the service in jndi

---
+++ ServiceLocator/Main.java
@@ -0,0 +1,14 @@
+package ServiceLocator;
+
+public class Main {
+    public static void main(String[] args) {
+        Service service = ServiceLocator.getService("Service1");
+        service.execute();
+        service = ServiceLocator.getService("Service2");
+        service.execute();
+        service = ServiceLocator.getService("Service1");
+        service.execute();
+        service = ServiceLocator.getService("Service2");
+        service.execute();
+    }
+}

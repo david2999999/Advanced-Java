faae6a8f92cd057166f4b896ddaaa6d7cece5c3f
David Jiang <djiang86@binghamton.edu>
2018-10-07 02:26:40 +0000

    coarse grained object and dependents

---
+++ CompositeEntity/DependentObject1.java
@@ -0,0 +1,13 @@
+package CompositeEntity;
+
+public class DependentObject1 {
+    private String data;
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+}

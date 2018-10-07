3ac52f5ba01fd390d3ff8f995bc5bad8c58e88ef
David Jiang <djiang86@binghamton.edu>
2018-10-07 03:37:21 +0000

    client sending request, filterchainManager executes the filter chain

---
+++ InteceptingFilter/Main.java
@@ -0,0 +1,13 @@
+package InteceptingFilter;
+
+public class Main {
+    public static void main(String[] args) {
+        FilterManager filterManager = new FilterManager(new Target());
+        filterManager.setFilter(new AuthenticationFilter());
+        filterManager.setFilter(new DebugFilter());
+
+        Client client = new Client();
+        client.setFilterManager(filterManager);
+        client.sendRequest("HOME");
+    }
+}

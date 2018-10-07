a2420a87c43069273fa251dc1faae20f53212531
David Jiang <djiang86@binghamton.edu>
2018-10-07 01:51:25 +0000

    changing execution algorithm with visitor class

--- Visitor/Computer.java
+++ Visitor/Computer.java
@@ -7,7 +7,7 @@ public class Computer implements ComputerPart {
     public Computer(){
         parts = new ComputerPart[] {new Mouse(), new Keyboard(), new Monitor()};
     }
-    
+
     @Override
     public void accept(ComputerPartVisitor computerPartVisitor) {
         for (int i = 0; i < parts.length; i++) {
---
+++ Visitor/Main.java
@@ -0,0 +1,8 @@
+package Visitor;
+
+public class Main {
+    public static void main(String[] args) {
+        ComputerPart computer = new Computer();
+        computer.accept(new ComputerPartDisplayVisitor());
+    }
+}

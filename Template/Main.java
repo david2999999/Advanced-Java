6f7f6002d8cf9f1091fc30ce8b82c6f3fec096e9
David Jiang <djiang86@binghamton.edu>
2018-10-07 01:49:56 +0000

    visitor pattern

--- Template/Main.java
+++ Template/Main.java
@@ -2,13 +2,12 @@ package Template;
 
 public class Main {
     public static void main(String[] args) {
-        public static void main(String[] args) {
 
-            Game game = new Cricket();
-            game.play();
-            System.out.println();
-            game = new Football();
-            game.play();
-        }
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
     }
+
 }

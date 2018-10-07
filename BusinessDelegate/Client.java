a29be43329ef61d797e7cc6b13fcacb49da52439
David Jiang <djiang86@binghamton.edu>
2018-10-07 02:15:07 +0000

    retrieving business service from client side

---
+++ BusinessDelegate/Client.java
@@ -0,0 +1,13 @@
+package BusinessDelegate;
+
+public class Client {
+    BusinessDelegate businessService;
+
+    public Client(BusinessDelegate businessService){
+        this.businessService  = businessService;
+    }
+
+    public void doTask(){
+        businessService.doTask();
+    }
+}
---
+++ BusinessDelegate/Main.java
@@ -0,0 +1,14 @@
+package BusinessDelegate;
+
+public class Main {
+    public static void main(String[] args) {
+        BusinessDelegate businessDelegate = new BusinessDelegate();
+        businessDelegate.setServiceType("EJB");
+
+        Client client = new Client(businessDelegate);
+        client.doTask();
+
+        businessDelegate.setServiceType("JMS");
+        client.doTask();
+    }
+}

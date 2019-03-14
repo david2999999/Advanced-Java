db1469240d1783bc5788e26e1f0a99ce4e75515b
David Jiang <djiang86@binghamton.edu>
2019-03-14 11:45:17 +0000

    class with one thread

---
+++ Java Threads PDF/Chapter 2 - The Java Threading API/SingleThread.java
@@ -0,0 +1,16 @@
+public class OurClass {
+    public void run() {
+        for (int I = 0; I < 100; I++) {
+            System.out.println("Hello");
+        }
+    }
+}
+
+import java.applet.Applet;
+
+public class OurApplet extends Applet {
+    public void init() {
+        OurClass oc = new OurClass();
+        oc.run();
+    }
+}

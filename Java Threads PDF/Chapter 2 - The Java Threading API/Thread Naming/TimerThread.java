37e684ec2059d146b5016b0f5e8914a162f1edfa
David Jiang <djiang86@binghamton.edu>
2019-03-15 10:48:31 +0000

    thread naming

---
+++ Java Threads PDF/Chapter 2 - The Java Threading API/Thread Naming/TimerThread.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class TimerThread extends Thread {
+    Component comp; // Component that needs repainting
+    int timediff; // Time between repaints of the component
+    volatile boolean shouldRun; // Set to false to stop thread
+    
+    public TimerThread(Component comp, int timediff) {
+        this.comp = comp;
+        this.timediff = timediff;
+        shouldRun = true;
+        setName("TimerThread(" + timediff + " milliseconds)");
+    }
+    public void run() {
+        while (shouldRun) {
+            try {
+                comp.repaint();
+                sleep(timediff);
+            } catch (Exception e) {}
+        }
+    }
+}

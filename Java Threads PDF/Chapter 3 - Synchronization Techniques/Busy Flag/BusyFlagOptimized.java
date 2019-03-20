7b861aa56c6fdc87a1b864734e4f527e04b25932
David Jiang <djiang86@binghamton.edu>
2019-03-20 11:41:46 +0000

    busy flag with synchronized

---
+++ Java Threads PDF/Chapter 3 - Synchronization Techniques/Busy Flag/BusyFlagOptimized.java
@@ -0,0 +1,24 @@
+public class BusyFlag {
+    protected Thread busyflag = null;
+    
+    public void getBusyFlag() {
+        while (tryGetBusyFlag() == false) {
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {}
+        }
+    }
+    
+    public synchronized boolean tryGetBusyFlag() {
+        if (busyflag == null) {
+            busyflag = Thread.currentThread();
+            return true;
+        }
+        return false;
+    }
+    public synchronized void freeBusyFlag() {
+        if (busyflag == Thread.currentThread()) {
+            busyflag = null;
+        }
+    }
+}

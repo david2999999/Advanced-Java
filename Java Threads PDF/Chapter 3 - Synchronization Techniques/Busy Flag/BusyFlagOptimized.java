25f58a60a09da01a34f89cbf94f9ef8283ed845f
David Jiang <djiang86@binghamton.edu>
2019-03-21 10:53:17 +0000

    using synchronize keyword to specify a specific block of code

--- Java Threads PDF/Chapter 3 - Synchronization Techniques/Busy Flag/BusyFlagOptimized.java
+++ Java Threads PDF/Chapter 3 - Synchronization Techniques/Busy Flag/BusyFlagOptimized.java
@@ -21,4 +21,19 @@ public class BusyFlag {
             busyflag = null;
         }
     }
+    
+    public void getBusyFlag () {
+        while (true) {
+            synchronized (this) {
+                if (busyflag == null) {
+                    busyflag = Thread.currentThread();
+                    break;
+                }
+            }
+            
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {}
+        }
+    }
 }

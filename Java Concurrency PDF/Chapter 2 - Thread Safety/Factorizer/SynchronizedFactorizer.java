153af77b2e348145b430079274cdecc4cad0f1df
David Jiang <djiang86@binghamton.edu>
2019-03-29 13:25:06 +0000

    synchronized factorization but with performance problem

---
+++ Java Concurrency PDF/Chapter 2 - Thread Safety/Factorizer/SynchronizedFactorizer.java
@@ -0,0 +1,17 @@
+@ThreadSafe
+public class SynchronizedFactorizer implements Servlet {
+    @GuardedBy("this") private BigInteger lastNumber;
+    @GuardedBy("this") private BigInteger[] lastFactors;
+    
+    public synchronized void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        if (i.equals(lastNumber))
+            encodeIntoResponse(resp, lastFactors);
+        else {
+            BigInteger[] factors = factor(i);
+            lastNumber = i;
+            lastFactors = factors;
+            encodeIntoResponse(resp, factors);
+        }
+    }
+}

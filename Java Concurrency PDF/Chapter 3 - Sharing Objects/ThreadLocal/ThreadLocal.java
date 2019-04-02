ef56be9094bf79b5ba849996b3740417cce42b0a
David Jiang <djiang86@binghamton.edu>
2019-04-02 11:25:48 +0000

    using thread local to prevent sharing in designs based on mutable singletons or global variables

---
+++ Java Concurrency PDF/Chapter 3 - Sharing Objects/ThreadLocal/ThreadLocal.java
@@ -0,0 +1,9 @@
+private static ThreadLocal<Connection> connectionHolder = new ThreadLocal<Connection>() {
+    public Connection initialValue() {
+        return DriverManager.getConnection(DB_URL);
+    }
+};
+
+public static Connection getConnection() {
+    return connectionHolder.get();
+}

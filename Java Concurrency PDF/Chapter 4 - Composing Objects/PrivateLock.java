52f6dd3f4cfacf36d61fbecd4117988b4b691d8e
David Jiang <djiang86@binghamton.edu>
2019-04-03 11:45:55 +0000

    java monitor pattern with private lock

---
+++ Java Concurrency PDF/Chapter 4 - Composing Objects/PrivateLock.java
@@ -0,0 +1,10 @@
+public class PrivateLock {
+    private final Object myLock = new Object();
+    @GuardedBy("myLock") Widget widget;
+    
+    void someMethod() {
+        synchronized(myLock) {
+        // Access or modify the state of widget
+        }
+    }
+}

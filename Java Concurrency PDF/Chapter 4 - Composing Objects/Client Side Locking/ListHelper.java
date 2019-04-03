11582e6c34acbe9e110673de4f23dbe996c774c2
David Jiang <djiang86@binghamton.edu>
2019-04-03 14:03:46 +0000

    client side locking

---
+++ Java Concurrency PDF/Chapter 4 - Composing Objects/Client Side Locking/ListHelper.java
@@ -0,0 +1,13 @@
+@ThreadSafe
+public class ListHelper<E> {
+    public List<E> list = Collections.synchronizedList(new ArrayList<E>());
+
+    public boolean putIfAbsent(E x) {
+        synchronized (list) {
+            boolean absent = !list.contains(x);
+            if (absent)
+                list.add(x);
+            return absent;
+        }
+    }
+}

ea1093fd9b55c42136f34919b7e3e829bad0d522
David Jiang <djiang86@binghamton.edu>
2019-04-03 11:28:37 +0000

    thread safe counter

---
+++ Java Concurrency PDF/Chapter 4 - Composing Objects/Counter.java
@@ -0,0 +1,13 @@
+@ThreadSafe
+public final class Counter {
+    @GuardedBy("this") private long value = 0;
+    
+    public synchronized long getValue() {
+        return value;
+    }
+    public synchronized long increment() {
+        if (value == Long.MAX_VALUE)
+            throw new IllegalStateException("counter overflow");
+        return ++value;
+    }
+}

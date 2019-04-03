d07f278bebdaf81d5ca0e7e26b4526531dd71a11
David Jiang <djiang86@binghamton.edu>
2019-04-03 11:39:46 +0000

    thread fafety through confinement

---
+++ Java Concurrency PDF/Chapter 4 - Composing Objects/PersonSet.java
@@ -0,0 +1,12 @@
+@ThreadSafe
+public class PersonSet {
+    @GuardedBy("this")
+    private final Set<Person> mySet = new HashSet<Person>();
+    
+    public synchronized void addPerson(Person p) {
+        mySet.add(p);
+    }
+    public synchronized boolean containsPerson(Person p) {
+        return mySet.contains(p);
+    }
+}

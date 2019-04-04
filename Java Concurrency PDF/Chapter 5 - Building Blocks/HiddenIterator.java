ba50e013c151f8ec0291d2c36e45bed554c16efc
David Jiang <djiang86@binghamton.edu>
2019-04-04 11:29:52 +0000

    hidden iterator in toString()

---
+++ Java Concurrency PDF/Chapter 5 - Building Blocks/HiddenIterator.java
@@ -0,0 +1,14 @@
+public class HiddenIterator {
+    @GuardedBy("this")
+    private final Set<Integer> set = new HashSet<Integer>();
+    
+    public synchronized void add(Integer i) { set.add(i); }
+    public synchronized void remove(Integer i) { set.remove(i); }
+    
+    public void addTenThings() {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++)
+            add(r.nextInt());
+        System.out.println("DEBUG: added ten elements to " + set);
+    }
+}

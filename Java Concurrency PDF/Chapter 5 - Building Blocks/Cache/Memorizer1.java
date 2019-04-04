db52085e9b69e15e7acdc4702acb05a3f96d4736
David Jiang <djiang86@binghamton.edu>
2019-04-04 20:34:24 +0000

    initial cache attempt

---
+++ Java Concurrency PDF/Chapter 5 - Building Blocks/Cache/Memorizer1.java
@@ -0,0 +1,18 @@
+public class Memorizer1<A, V> implements Computable<A, V> {
+    @GuardedBy("this")
+    private final Map<A, V> cache = new HashMap<A, V>();
+    private final Computable<A, V> c;
+    
+    public Memorizer1(Computable<A, V> c) {
+        this.c = c;
+    }
+    
+    public synchronized V compute(A arg) throws InterruptedException {
+        V result = cache.get(arg);
+        if (result == null) {
+            result = c.compute(arg);
+            cache.put(arg, result);
+        }
+        return result;
+    }
+}

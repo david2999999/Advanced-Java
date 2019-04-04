bca8bcb0313792bcebecd6971187946ee929792f
David Jiang <djiang86@binghamton.edu>
2019-04-04 12:46:26 +0000

    file crawler

---
+++ Java Concurrency PDF/Chapter 5 - Building Blocks/Desktop Search/FileCrawler.java
@@ -0,0 +1,23 @@
+public class FileCrawler implements Runnable {
+    private final BlockingQueue<File> fileQueue;
+    private final FileFilter fileFilter;
+    private final File root;
+    
+    public void run() {
+        try {
+            crawl(root);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    private void crawl(File root) throws InterruptedException {
+        File[] entries = root.listFiles(fileFilter);
+        if (entries != null) {
+            for (File entry : entries)
+                if (entry.isDirectory())
+                    crawl(entry);
+        else if (!alreadyIndexed(entry))
+            fileQueue.put(entry);
+        }
+    }
+}

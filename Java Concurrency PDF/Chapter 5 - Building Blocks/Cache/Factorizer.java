3fc4eb95c6d5ddaf7f45dcb734325333ad847182
David Jiang <djiang86@binghamton.edu>
2019-04-05 13:55:48 +0000

    factorizer with cache

---
+++ Java Concurrency PDF/Chapter 5 - Building Blocks/Cache/Factorizer.java
@@ -0,0 +1,19 @@
+@ThreadSafe
+public class Factorizer implements Servlet {
+    private final Computable<BigInteger, BigInteger[]> c = new Computable<BigInteger, BigInteger[]>() {
+        public BigInteger[] compute(BigInteger arg) {
+            return factor(arg);
+        }
+    };
+    
+    private final Computable<BigInteger, BigInteger[]> cache = new Memorizer<BigInteger, BigInteger[]>(c);
+    
+    public void service(ServletRequest req, ServletResponse resp) {
+        try {
+            BigInteger i = extractFromRequest(req);
+            encodeIntoResponse(resp, cache.compute(i));
+        } catch (InterruptedException e) {
+            encodeError(resp, "factorization interrupted");
+        }
+    }
+}

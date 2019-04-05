219a2b656b683d452519546947c70bb1d0cc43ba
David Jiang <djiang86@binghamton.edu>
2019-04-05 13:40:58 +0000

    cache with concurrent hashmap

---
+++ Java Concurrency PDF/Chapter 5 - Building Blocks/Cache/Second Cache Attempt/Memorizer2.java
@@ -0,0 +1,16 @@
+public class Memorizer2<A, V> implements Computable<A, V> {
+    private final Map<A, V> cache = new ConcurrentHashMap<A, V>();
+    private final Computable<A, V> c;
+    
+    public Memorizer2(Computable<A, V> c) { this.c = c; }
+    
+    public V compute(A arg) throws InterruptedException {
+        V result = cache.get(arg);
+        
+        if (result == null) {
+            result = c.compute(arg);
+            cache.put(arg, result);
+        }
+        return result;
+    }
+}

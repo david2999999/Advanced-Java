5062f86e9160d556acdae5cbe58aa2969a673c3f
David Jiang <djiang86@binghamton.edu>
2019-04-05 14:06:30 +0000

    thread per task web server

---
+++ Java Concurrency PDF/Chapter 6 - Task Execution/ThreadPerTaskWebServer.java
@@ -0,0 +1,15 @@
+class ThreadPerTaskWebServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        
+        while (true) {
+            final Socket connection = socket.accept();
+            Runnable task = new Runnable() {
+                public void run() {
+                    handleRequest(connection);
+                }
+            };
+            new Thread(task).start();
+        }
+    }
+}

13e156e059804d830716ff931715e87ad4de9848
David Jiang <djiang86@binghamton.edu>
2019-04-08 12:31:11 +0000

    future interface

---
+++ Java Concurrency PDF/Chapter 6 - Task Execution/Future.java
@@ -0,0 +1,10 @@
+public interface Future<V> {
+    boolean cancel(boolean mayInterruptIfRunning);
+    boolean isCancelled();
+    boolean isDone();
+    
+    V get() throws InterruptedException, ExecutionException, CancellationException;
+    V get(long timeout, TimeUnit unit)
+        throws InterruptedException, ExecutionException,
+        CancellationException, TimeoutException;
+}

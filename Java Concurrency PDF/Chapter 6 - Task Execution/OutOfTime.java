85dd3dfae759a8ea71b2a3c792aa5d166e22a619
David Jiang <djiang86@binghamton.edu>
2019-04-08 12:10:38 +0000

    confusing timer behavior

---
+++ Java Concurrency PDF/Chapter 6 - Task Execution/OutOfTime.java
@@ -0,0 +1,13 @@
+public class OutOfTime {
+    public static void main(String[] args) throws Exception {
+        Timer timer = new Timer();
+        timer.schedule(new ThrowTask(), 1);
+        SECONDS.sleep(1);
+        timer.schedule(new ThrowTask(), 1);
+        SECONDS.sleep(5);
+    }
+    
+    static class ThrowTask extends TimerTask {
+        public void run() { throw new RuntimeException(); }
+    }
+}

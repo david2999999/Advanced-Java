4cc500d57e56e08a794cde224d53b327e02f2507
David Jiang <djiang86@binghamton.edu>
2019-04-09 12:28:25 +0000

    non cancelable task that restores interruption before exit

---
+++ Java Concurrency PDF/Chapter 7 - Cancellation and Shutdown/NoncancelableThread.java
@@ -0,0 +1,18 @@
+public class NoncancelableThread {
+    public Task getNextTask(BlockingQueue<Taskgt; queue) {
+        boolean interrupted = false;
+        try {
+            while (true) {
+                try {
+                    return queue.take();
+                } catch (InterruptedException e) {
+                    interrupted = true;
+                    // fall through and retry
+                }
+            }
+        } finally {
+            if (interrupted)
+                Thread.currentThread().interrupt();
+        }
+    }
+}

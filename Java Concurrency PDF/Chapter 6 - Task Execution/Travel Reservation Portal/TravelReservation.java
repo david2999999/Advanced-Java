85d0b39a2fee5493f825b31158b66444158770e2
David Jiang <djiang86@binghamton.edu>
2019-04-09 11:14:55 +0000

    get travel reservation quotes with invodeAll

---
+++ Java Concurrency PDF/Chapter 6 - Task Execution/Travel Reservation Portal/TravelReservation.java
@@ -0,0 +1,26 @@
+public class TravelReservation {
+    public List<TravelQuote> getRankedTravelQuotes( TravelInfo travelInfo, Set<TravelCompany> companies,
+            Comparator<TravelQuote> ranking, long time, TimeUnit unit) throws InterruptedException {
+
+        List<QuoteTask> tasks = new ArrayList<QuoteTask>();
+        for (TravelCompany company : companies)
+            tasks.add(new QuoteTask(company, travelInfo));
+            
+        List<Future<TravelQuote>> futures = exec.invokeAll(tasks, time, unit);
+        List<TravelQuote> quotes = new ArrayList<TravelQuote>(tasks.size());
+        Iterator<QuoteTask> taskIter = tasks.iterator();
+        
+        for (Future<TravelQuote> f : futures) {
+            QuoteTask task = taskIter.next();
+            try {
+                quotes.add(f.get());
+            } catch (ExecutionException e) {
+                quotes.add(task.getFailureQuote(e.getCause()));
+            } catch (CancellationException e) {
+                quotes.add(task.getTimeoutQuote(e));
+            }
+        }
+        Collections.sort(quotes, ranking);
+        return quotes;
+    }
+}

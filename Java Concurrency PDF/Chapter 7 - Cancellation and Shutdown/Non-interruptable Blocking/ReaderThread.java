2c5b053fb29ec77d86dd0e7c48a9517cb8e0b321
David Jiang <djiang86@binghamton.edu>
2019-04-10 11:58:27 +0000

    encapsulating nonstandard cancellation in a thread by overriding interrupt

---
+++ Java Concurrency PDF/Chapter 7 - Cancellation and Shutdown/Non-interruptable Blocking/ReaderThread.java
@@ -0,0 +1,32 @@
+public class ReaderThread extends Thread {
+    private final Socket socket;
+    private final InputStream in;
+    
+    public ReaderThread(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = socket.getInputStream();
+    }
+    
+    public void interrupt() {
+        try {
+            socket.close();
+        }
+        catch (IOException ignored) { }
+        finally {
+            super.interrupt();
+        }
+    }
+    
+    public void run() {
+        try {
+            byte[] buf = new byte[BUFSZ];
+            while (true) {
+                int count = in.read(buf);
+                if (count < 0)
+                break;
+                else if (count > 0)
+                processBuffer(buf, count);
+            }
+        } catch (IOException e) { /* Allow thread to exit */ }
+    }
+}

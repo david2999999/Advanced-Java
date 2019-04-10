cdf6e9095477509c24660e96e276a849a6180e15
David Jiang <djiang86@binghamton.edu>
2019-04-10 13:33:49 +0000

    Encapsulating nonstandard cancellation in a task with newtaskfor

---
+++ Java Concurrency PDF/Chapter 7 - Cancellation and Shutdown/Encapsulating Nonstandard Cancellation in a Task/SocketUsingTask.java
@@ -0,0 +1,25 @@
+public abstract class SocketUsingTask<T>
+    implements CancellableTask<T> {
+    @GuardedBy("this") private Socket socket;
+    
+    protected synchronized void setSocket(Socket s) { socket = s; }
+    
+    public synchronized void cancel() {
+        try {
+            if (socket != null)
+                socket.close();
+        } catch (IOException ignored) { }
+    }
+    
+    public RunnableFuture<T> newTask() {
+        return new FutureTask<T>(this) {
+            public boolean cancel(boolean mayInterruptIfRunning) {
+                try {
+                    SocketUsingTask.this.cancel();
+                } finally {
+                    return super.cancel(mayInterruptIfRunning);
+                }
+            }
+        };
+    }
+}

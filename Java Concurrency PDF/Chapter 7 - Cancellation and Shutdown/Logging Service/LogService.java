ff818ae969c91a809c4c8b52e980e7bd9c8f0ce8
David Jiang <djiang86@binghamton.edu>
2019-04-11 11:31:25 +0000

    reliable cancellation to logging service

---
+++ Java Concurrency PDF/Chapter 7 - Cancellation and Shutdown/Logging Service/LogService.java
@@ -0,0 +1,43 @@
+public class LogService {
+    private final BlockingQueue<String> queue;
+    private final LoggerThread loggerThread;
+    private final PrintWriter writer;
+    @GuardedBy("this") private boolean isShutdown;
+    @GuardedBy("this") private int reservations;
+    
+    public void start() { loggerThread.start(); }
+    
+    public void stop() {
+        synchronized (this) { isShutdown = true; }
+        loggerThread.interrupt();
+    }
+    
+    public void log(String msg) throws InterruptedException {
+        synchronized (this) {
+            if (isShutdown)
+                throw new IllegalStateException(...);
+            ++reservations;
+        }
+        queue.put(msg);
+    }
+    
+    private class LoggerThread extends Thread {
+        public void run() {
+            try {
+                while (true) {
+                    try {
+                        synchronized (this) {
+                            if (isShutdown && reservations == 0)
+                                break;
+                        }
+                        String msg = queue.take();
+                        synchronized (this) { --reservations; }
+                        writer.println(msg);
+                    } catch (InterruptedException e) { /* retry */ }
+                }
+            } finally {
+                writer.close();
+            }
+        }
+    }
+}

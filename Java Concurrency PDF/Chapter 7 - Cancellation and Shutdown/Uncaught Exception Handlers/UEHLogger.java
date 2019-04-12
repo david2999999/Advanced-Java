11f4092b9e2573c71f16800e703702c92bc73966
David Jiang <djiang86@binghamton.edu>
2019-04-12 11:51:18 +0000

    Elegant way to handle run time exceptions

---
+++ Java Concurrency PDF/Chapter 7 - Cancellation and Shutdown/Uncaught Exception Handlers/UEHLogger.java
@@ -0,0 +1,6 @@
+public class UEHLogger implements Thread.UncaughtExceptionHandler {
+    public void uncaughtException(Thread t, Throwable e) {
+        Logger logger = Logger.getAnonymousLogger();
+        logger.log(Level.SEVERE, "Thread terminated with exception: " + t.getName(), e);
+    }
+}

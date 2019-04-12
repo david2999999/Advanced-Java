9330b8d1f848f706d735a570372c4ecf17af193c
David Jiang <djiang86@binghamton.edu>
2019-04-12 11:25:13 +0000

    web crawler that keeps track of cancelled tasks

---
+++ Java Concurrency PDF/Chapter 7 - Cancellation and Shutdown/Keeping Track of Cancelled Tasks After Shutdown/WebCrawler.java
@@ -0,0 +1,33 @@
+public abstract class WebCrawler {
+    private volatile TrackingExecutor exec;
+    @GuardedBy("this")
+    private final Set<URL> urlsToCrawl = new HashSet<URL>();
+
+    public synchronized void start() {
+        exec = new TrackingExecutor(
+        Executors.newCachedThreadPool());
+        for (URL url : urlsToCrawl) submitCrawlTask(url);
+            urlsToCrawl.clear();
+    }
+    
+    public synchronized void stop() throws InterruptedException {
+        try {
+            saveUncrawled(exec.shutdownNow());
+            if (exec.awaitTermination(TIMEOUT, UNIT))
+                saveUncrawled(exec.getCancelledTasks());
+        } finally {
+            exec = null;
+        }
+    }
+    protected abstract List<URL> processPage(URL url);
+    
+    private void saveUncrawled(List<Runnable> uncrawled) {
+        for (Runnable task : uncrawled)
+            urlsToCrawl.add(((CrawlTask) task).getPage());
+    }
+    
+    private void submitCrawlTask(URL u) {
+        exec.execute(new CrawlTask(u));
+    }
+
+}

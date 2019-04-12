a9fa6239560b5dba3719363db57a560a5f6a1992
David Jiang <djiang86@binghamton.edu>
2019-04-12 10:51:46 +0000

    keep tracks o fcancelled tasks after shutdown

---
+++ Java Concurrency PDF/Chapter 7 - Cancellation and Shutdown/Keeping Track of Cancelled Tasks After Shutdown/TrackingExecutor.java
@@ -0,0 +1,24 @@
+public class TrackingExecutor extends AbstractExecutorService {
+    private final ExecutorService exec;
+    private final Set<Runnable> tasksCancelledAtShutdown = Collections.synchronizedSet(new HashSet<Runnable>());
+
+    public List<Runnable> getCancelledTasks() {
+        if (!exec.isTerminated())
+            throw new IllegalStateException(...);
+        return new ArrayList<Runnable>(tasksCancelledAtShutdown);
+    }
+    
+    public void execute(final Runnable runnable) {
+        exec.execute(new Runnable() {
+            public void run() {
+                try {
+                    runnable.run();
+                } finally {
+                    if (isShutdown() && Thread.currentThread().isInterrupted())
+                        tasksCancelledAtShutdown.add(runnable);
+                }
+            }
+        });
+    }
+    // delegate other ExecutorService methods to exec
+}

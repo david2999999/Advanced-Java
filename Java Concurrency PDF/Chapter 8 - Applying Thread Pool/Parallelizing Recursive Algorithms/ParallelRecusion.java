cbe23d05aecbab5829dbbde0964fdcc18d950e8d
David Jiang <djiang86@binghamton.edu>
2019-04-15 12:08:40 +0000

    parallel recursion

---
+++ Java Concurrency PDF/Chapter 8 - Applying Thread Pool/Parallelizing Recursive Algorithms/ParallelRecusion.java
@@ -0,0 +1,20 @@
+public class ParallelRecusion {
+    public<T> void sequentialRecursive(List<Node<T>> nodes, Collection<T> results) {
+        for (Node<T> n : nodes) {
+            results.add(n.compute());
+            sequentialRecursive(n.getChildren(), results);
+        }
+    }
+    
+    public<T> void parallelRecursive(final Executor exec, List<Node<T>> nodes, final Collection<T> results) {
+        for (final Node<T> n : nodes) {
+            exec.execute(new Runnable() {
+                public void run() {
+                    results.add(n.compute());
+                }
+            });
+            
+            parallelRecursive(exec, n.getChildren(), results);
+        }
+    }
+}

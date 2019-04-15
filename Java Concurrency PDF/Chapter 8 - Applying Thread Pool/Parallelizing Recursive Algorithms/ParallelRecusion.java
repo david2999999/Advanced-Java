4227d268d91f8421f2938adc1b42d4cda6d36d0a
David Jiang <djiang86@binghamton.edu>
2019-04-15 12:10:12 +0000

    waiting for results to be calculated in paralle

--- Java Concurrency PDF/Chapter 8 - Applying Thread Pool/Parallelizing Recursive Algorithms/ParallelRecusion.java
+++ Java Concurrency PDF/Chapter 8 - Applying Thread Pool/Parallelizing Recursive Algorithms/ParallelRecusion.java
@@ -17,4 +17,13 @@ public class ParallelRecusion {
             parallelRecursive(exec, n.getChildren(), results);
         }
     }
+    
+    public<T> Collection<T> getParallelResults(List<Node<T>> nodes) throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Queue<T> resultQueue = new ConcurrentLinkedQueue<T>();
+        parallelRecursive(exec, nodes, resultQueue);
+        exec.shutdown();
+        exec.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        return resultQueue;
+    }
 }

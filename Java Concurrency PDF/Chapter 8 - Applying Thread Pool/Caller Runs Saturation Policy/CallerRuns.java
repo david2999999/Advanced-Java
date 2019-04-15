dd723978faebf440ed44a3c3e02e00831853ae1d
David Jiang <djiang86@binghamton.edu>
2019-04-15 11:25:56 +0000

    allowing caller to do the work if the bounded work queue fills up

---
+++ Java Concurrency PDF/Chapter 8 - Applying Thread Pool/Caller Runs Saturation Policy/CallerRuns.java
@@ -0,0 +1,6 @@
+public class CallerRuns {
+    ThreadPoolExecutor executor = 
+        new ThreadPoolExecutor(N_THREADS, N_THREADS, 0L, TimeUnit.MILLISECONDS,
+                                    new LinkedBlockingQueue<Runnable>(CAPACITY));
+    executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+}

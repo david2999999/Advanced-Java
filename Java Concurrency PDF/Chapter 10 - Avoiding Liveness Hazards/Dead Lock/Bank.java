6e9a99a7bbb590f5743b960e96d582cd710c74ee
David Jiang <djiang86@binghamton.edu>
2019-04-17 12:50:41 +0000

    transfer money deadlock

---
+++ Java Concurrency PDF/Chapter 10 - Avoiding Liveness Hazards/Dead Lock/Bank.java
@@ -0,0 +1,15 @@
+public class Bank {
+    public void transferMoney(Account fromAccount, Account toAccount, DollarAmount amount)
+                throws InsufficientFundsException {
+        synchronized (fromAccount) {
+            synchronized (toAccount) {
+                if (fromAccount.getBalance().compareTo(amount) < 0)
+                    throw new InsufficientFundsException();
+                else {
+                    fromAccount.debit(amount);
+                    toAccount.credit(amount);
+                }
+            }
+        }
+    }
+}

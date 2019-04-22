e69a1598728cb66e1225c7eb47c842e2ef9825ed
David Jiang <djiang86@binghamton.edu>
2019-04-22 11:57:05 +0000

    lock elision

---
+++ Java Concurrency PDF/Chapter 11 - Performance and Scalability/Lock Elision/getStoogeNames.java
@@ -0,0 +1,7 @@
+public String getStoogeNames() {
+    List<String> stooges = new Vector<String>();
+    stooges.add("Moe");
+    stooges.add("Larry");
+    stooges.add("Curly");
+    return stooges.toString();
+}

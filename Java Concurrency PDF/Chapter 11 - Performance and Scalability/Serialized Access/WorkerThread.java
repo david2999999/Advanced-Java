4f6ca2fb0690633362793fadcadfe3810d28fda8
David Jiang <djiang86@binghamton.edu>
2019-04-22 10:55:25 +0000

    serialized access to a task queue

---
+++ Java Concurrency PDF/Chapter 11 - Performance and Scalability/Serialized Access/WorkerThread.java
@@ -0,0 +1,18 @@
+public class WorkerThread extends Thread {
+    private final BlockingQueue<Runnable> queue;
+    
+    public WorkerThread(BlockingQueue<Runnable> queue) {
+        this.queue = queue;
+    }
+    
+    public void run() {
+        while (true) {
+            try {
+                Runnable task = queue.take();
+                task.run();
+            } catch (InterruptedException e) {
+                break; /* Allow thread to exit */
+            }
+        }
+    }
+}

501a78fbece8ee1fae44d4bb2fa0d1be4a94ca46
David Jiang <djiang86@binghamton.edu>
2019-04-24 12:37:01 +0000

    producer consumer test with barrier

---
+++ Java Concurrency PDF/Chapter 12 - Testing Concurrent Programs/Producer Consumer Test Program/PutTakeTest.java
@@ -0,0 +1,36 @@
+public class PutTakeTest {
+    private static final ExecutorService pool = Executors.newCachedThreadPool();
+    private final AtomicInteger putSum = new AtomicInteger(0);
+    private final AtomicInteger takeSum = new AtomicInteger(0);
+    private final CyclicBarrier barrier;
+    private final BoundedBuffer<Integer> bb;
+    private final int nTrials, nPairs;
+    
+    public static void main(String[] args) {
+        new PutTakeTest(10, 10, 100000).test(); // sample parameters
+        pool.shutdown();
+    }
+    
+    PutTakeTest(int capacity, int npairs, int ntrials) {
+        this.bb = new BoundedBuffer<Integer>(capacity);
+        this.nTrials = ntrials;
+        this.nPairs = npairs;
+        this.barrier = new CyclicBarrier(npairs* 2 + 1);
+    }
+    
+    void test() {
+        try {
+            for (int i = 0; i < nPairs; i++) {
+                pool.execute(new Producer());
+                pool.execute(new Consumer());
+            }
+            barrier.await(); // wait for all threads to be ready
+            barrier.await(); // wait for all threads to finish
+            assertEquals(putSum.get(), takeSum.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    class Producer implements Runnable { /* Listing 12.6 */ }
+    class Consumer implements Runnable { /* Listing 12.6 */ }
+}

af418335791f0b5bd67cb8d0244cc6e31310a554
David Jiang <djiang86@binghamton.edu>
2019-04-26 13:58:01 +0000

    avoiding deadlock using trylock

---
+++ Java Concurrency PDF/Chapter 13 - Explicit Locks/Avoiding Deadlock Using tryLock/TransferMoney.java
@@ -0,0 +1,36 @@
+public boolean transferMoney(Account fromAcct,
+                            Account toAcct,
+                            DollarAmount amount,
+                            long timeout,
+                            TimeUnit unit) throws InsufficientFundsException, InterruptedException {
+    long fixedDelay = getFixedDelayComponentNanos(timeout, unit);
+    long randMod = getRandomDelayModulusNanos(timeout, unit);
+    long stopTime = System.nanoTime() + unit.toNanos(timeout);
+    
+    while (true) {
+        if (fromAcct.lock.tryLock()) {
+            try {
+                if (toAcct.lock.tryLock()) {
+                    try {
+                        if (fromAcct.getBalance().compareTo(amount) < 0)
+                            throw new InsufficientFundsException();
+                        else {
+                            fromAcct.debit(amount);
+                            toAcct.credit(amount);
+                            return true;
+                        }
+                    } finally {
+                        toAcct.lock.unlock();
+                    }
+                }
+            } finally {
+                fromAcct.lock.unlock();
+            }
+        }
+        
+        if (System.nanoTime() < stopTime)
+            return false;
+            
+        NANOSECONDS.sleep(fixedDelay + rnd.nextLong() % randMod);
+    }
+}

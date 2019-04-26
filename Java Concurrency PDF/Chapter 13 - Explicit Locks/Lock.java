9202901b105cde6877a839658543eca5a203e3ce
David Jiang <djiang86@binghamton.edu>
2019-04-26 13:17:54 +0000

    lock interface

---
+++ Java Concurrency PDF/Chapter 13 - Explicit Locks/Lock.java
@@ -0,0 +1,8 @@
+public interface Lock {
+    void lock();
+    void lockInterruptibly() throws InterruptedException;
+    boolean tryLock();
+    boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException;
+    void unlock();
+    Condition newCondition();
+}

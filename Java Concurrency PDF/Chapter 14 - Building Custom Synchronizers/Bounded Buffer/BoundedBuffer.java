a6031583ecb0d17ca281409695c484566613d1e6
David Jiang <djiang86@binghamton.edu>
2019-04-29 12:04:54 +0000

    bounded buffer with wait and notifyAll

---
+++ Java Concurrency PDF/Chapter 14 - Building Custom Synchronizers/Bounded Buffer/BoundedBuffer.java
@@ -0,0 +1,23 @@
+@ThreadSafe
+public class BoundedBuffer<V> extends BaseBoundedBuffer<V> {
+    // CONDITION PREDICATE: not-full (!isFull())
+    // CONDITION PREDICATE: not-empty (!isEmpty())
+    public BoundedBuffer(int size) { super(size); }
+    
+    // BLOCKS-UNTIL: not-full
+    public synchronized void put(V v) throws InterruptedException {
+        while (isFull())
+            wait();
+        doPut(v);
+        notifyAll();
+    }
+    
+    // BLOCKS-UNTIL: not-empty
+    public synchronized V take() throws InterruptedException {
+        while (isEmpty())
+            wait();
+        V v = doTake();
+        notifyAll();
+        return v;
+    }
+}

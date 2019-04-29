fa1925708f4a66b0d6d3156f9459c96858937aa3
David Jiang <djiang86@binghamton.edu>
2019-04-29 11:45:55 +0000

    sleepy bounded buffer

---
+++ Java Concurrency PDF/Chapter 14 - Building Custom Synchronizers/Bounded Buffer/SleepyBoundedBuffer.java
@@ -0,0 +1,26 @@
+@ThreadSafe
+public class SleepyBoundedBuffer<V> extends BaseBoundedBuffer<V> {
+    public SleepyBoundedBuffer(int size) { super(size); }
+    
+    public void put(V v) throws InterruptedException {
+        while (true) {
+            synchronized (this) {
+                if (!isFull()) {
+                    doPut(v);
+                    return;
+                }
+            }
+            Thread.sleep(SLEEP_GRANULARITY);
+        }
+    }
+    
+    public V take() throws InterruptedException {
+        while (true) {
+            synchronized (this) {
+                if (!isEmpty())
+                    return doTake();
+            }
+            Thread.sleep(SLEEP_GRANULARITY);
+        }
+    }
+}

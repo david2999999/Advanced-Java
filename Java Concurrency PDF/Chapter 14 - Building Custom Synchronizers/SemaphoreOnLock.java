573f37db060f2456ca67ea34f75a3d200ae41c0d
David Jiang <djiang86@binghamton.edu>
2019-04-30 20:53:15 +0000

    semaphore on lock

---
+++ Java Concurrency PDF/Chapter 14 - Building Custom Synchronizers/SemaphoreOnLock.java
@@ -0,0 +1,39 @@
+@ThreadSafe
+public class SemaphoreOnLock {
+    private final Lock lock = new ReentrantLock();
+    // CONDITION PREDICATE: permitsAvailable (permits > 0)
+    private final Condition permitsAvailable = lock.newCondition();
+    
+    @GuardedBy("lock") private int permits;
+    
+    SemaphoreOnLock(int initialPermits) {
+        lock.lock();
+        try {
+            permits = initialPermits;
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    // BLOCKS-UNTIL: permitsAvailable
+    public void acquire() throws InterruptedException {
+        lock.lock();
+        try {
+            while (permits <= 0)
+                permitsAvailable.await();
+            --permits;
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public void release() {
+        lock.lock();
+        try {
+            ++permits;
+            permitsAvailable.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}

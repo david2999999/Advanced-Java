3c2bad59697246e4611d5de5e86a380b808b262e
David Jiang <djiang86@binghamton.edu>
2019-05-02 11:22:39 +0000

    yield in applet

---
+++ Java Threads PDF/Chapter 6 - Java Thread Scheduling/Yield/Fractal.java
@@ -0,0 +1,49 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Fractal extends Applet implements Runnable {
+    Thread t;
+    volatile boolean shouldRun = false;
+    Object runLock = new Object();
+    int nSections;
+    
+    public void start() {
+        if (t == null) {
+            shouldRun = true;
+            t = new Thread(this);
+            t.start();
+        }
+        else {
+            synchronized(runLock) {
+                shouldRun = true;
+                runLock.notify();
+            }
+        }
+    }
+    
+    public void stop() {
+        shouldRun = false;
+    }
+    
+    void doCalc(int i) {
+        // Calculate the ith section of the fractal.
+    }
+    
+    public void run() {
+        for (int i = 0; i < nSections; i++) {
+            doCalc(i);
+            repaint();
+            Thread.yield();
+            synchronized(runLock) {
+                while (shouldRun == false)
+                    try {
+                        runLock.wait();
+                    } catch (InterruptedException ie) {}
+            }
+        }
+    }
+    
+    public void paint(Graphics g) {
+        // Paint the completed sections of the fractal.
+    }
+}

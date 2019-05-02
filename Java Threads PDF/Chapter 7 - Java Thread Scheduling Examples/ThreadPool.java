3d367239df53be89c0cf65a69e904ff738d12bd5
David Jiang <djiang86@binghamton.edu>
2019-05-02 12:50:50 +0000

    thread pool thread inner class

--- Java Threads PDF/Chapter 7 - Java Thread Scheduling Examples/ThreadPool.java
+++ Java Threads PDF/Chapter 7 - Java Thread Scheduling Examples/ThreadPool.java
@@ -10,4 +10,65 @@ public class ThreadPool {
         }
     }
     
+    class ThreadPoolThread extends Thread {
+        ThreadPool parent;
+        volatile boolean shouldRun = true;
+        
+        ThreadPoolThread(ThreadPool parent, int i) {
+            super("ThreadPoolThread " + i);
+            this.parent = parent;
+        }
+        
+        public void run() {
+            ThreadPoolRequest obj = null;
+            while (shouldRun) {
+                try {
+                    parent.cvFlag.getBusyFlag();
+                    while (obj == null && shouldRun) {
+                        try {
+                            obj = (ThreadPoolRequest)
+                            parent.objects.elementAt(0);
+                            parent.objects.removeElementAt(0);
+                        } catch (ArrayIndexOutOfBoundsException aiobe) {
+                            obj = null;
+                        } catch (ClassCastException cce) {
+                            System.err.println("Unexpected data");
+                            obj = null;
+                        }
+                        
+                        if (obj == null) {
+                            try {
+                                parent.cvAvailable.cvWait();
+                            } catch (InterruptedException ie) {
+                                return;
+                            }
+                        }
+                    }
+                } finally {
+                    parent.cvFlag.freeBusyFlag();
+                }
+                
+                if (!shouldRun)
+                    return;
+                    
+                obj.target.run();
+                try {
+                    parent.cvFlag.getBusyFlag();
+                    nObjects--;
+                    
+                    if (nObjects == 0)
+                        parent.cvEmpty.cvSignal();
+                } finally {
+                    parent.cvFlag.freeBusyFlag();
+                }
+                
+                if (obj.lock != null) {
+                    synchronized(obj.lock) {
+                        obj.lock.notify();
+                    }
+                }
+                obj = null;
+            }
+        }
+    }
 }

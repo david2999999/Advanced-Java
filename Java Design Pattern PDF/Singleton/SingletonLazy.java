ce1735303d5a86e648366cacd34ab7768d595578
david2999999 <djiang86@binghamton.edu>
2019-05-06 17:43:21 +0000

    singleton lazy loading

---
+++ Java Design Pattern PDF/Singleton/SingletonLazy.java
@@ -0,0 +1,11 @@
+public class SingletonLazy {
+    private static SingletonLazy sc = null;
+    private SingletonLazy(){}
+    
+    public static SingletonLazy getInstance(){
+        if(sc==null){
+            sc = new SingletonLazy();
+        }
+        return sc;
+    }
+}

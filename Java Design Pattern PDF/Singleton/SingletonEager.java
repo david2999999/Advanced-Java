96af630af00737a4bf3cc038bf661b78bc8e77bb
david2999999 <djiang86@binghamton.edu>
2019-05-06 17:42:54 +0000

    singleton eager loading

---
+++ Java Design Pattern PDF/Singleton/SingletonEager.java
@@ -0,0 +1,8 @@
+public class SingletonEager {
+    private static SingletonEager sc = new SingletonEager();
+    private SingletonEager(){}
+    
+    public static SingletonEager getInstance(){
+        return sc;
+    }
+}

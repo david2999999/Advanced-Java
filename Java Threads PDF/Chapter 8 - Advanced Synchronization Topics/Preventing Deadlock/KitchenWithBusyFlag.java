c7c66f2ac7a4bae4b38e13cc21e17b0a41e97e9d
david2999999 <djiang86@binghamton.edu>
2019-05-09 20:48:39 +0000

    deadlock prevention with busy flag

---
+++ Java Threads PDF/Chapter 8 - Advanced Synchronization Topics/Preventing Deadlock/KitchenWithBusyFlag.java
@@ -0,0 +1,26 @@
+public class Kitchen {
+    static MeasuringCup theCup;
+    static Bowl theBowl;
+    static BusyFlag theCupFlag, theBowlFlag;
+    
+    public void makeCookie() {
+        theCupFlag.getBusyFlag();
+        theCup.measureOut(1, theFlour);
+        theBowlFlag.getBusyFlag();
+        theBowl.putIngredients(theCup);
+        theBowl.mix();
+        theBowlFlag.freeBusyFlag();
+        theCupFlag.freeBusyFlag();
+    }
+    
+    public void makeOmelette() {
+        theBowlFlag.getBusyFlag();
+        Eggs e[] = getBrokenEggs();
+        theBowl.putIngredients(e);
+        theBowl.mix();
+        theCupFlag.getBusyFlag();
+        theCup.measureOut(theBowl);
+        theCupFlag.freeBusyFlag();
+        theBowlFlag.freeBusyFlag();
+    }
+}

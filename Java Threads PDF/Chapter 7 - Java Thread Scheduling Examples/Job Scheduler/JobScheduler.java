5dfb5dc22bfa50d9240245c2514b0fea1feee33a
david2999999 <djiang86@binghamton.edu>
2019-05-09 19:43:46 +0000

    executing job in job scheduler

--- Java Threads PDF/Chapter 7 - Java Thread Scheduling Examples/Job Scheduler/JobScheduler.java
+++ Java Threads PDF/Chapter 7 - Java Thread Scheduling Examples/Job Scheduler/JobScheduler.java
@@ -31,13 +31,107 @@ public class JobScheduler implements Runnable {
         jobs.addElement(job);
         notify();
     }
+    
     private synchronized void deleteJob(Runnable job) {
         for (int i=0; i < jobs.size(); i++) {
-        if (((JobNode) jobs.elementAt(i)).job == job) {
-            jobs.removeElementAt(i);
-            dlock.release();
-            break;
+            if (((JobNode) jobs.elementAt(i)).job == job) {
+                jobs.removeElementAt(i);
+                dlock.release();
+                break;
+            }
         }
     }
-}
+    
+    private JobNode updateJobNode(JobNode jn) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(jn.executeAt);
+        if (jn.interval == MONTHLY) {
+            // There is a minor bug (see java.util.calendar).
+            cal.add(Calendar.MONTH, 1);
+            jn.executeAt = cal.getTime();
+        } else if (jn.interval == YEARLY) {
+            cal.add(Calendar.YEAR, 1);
+            jn.executeAt = cal.getTime();
+        } else {
+            jn.executeAt = new Date(jn.executeAt.getTime() + jn.interval);
+        }
+        
+        jn.count = (jn.count == FOREVER) ? FOREVER : jn.count - 1;
+        return (jn.count != 0) ? jn : null;
+    }
+    
+    private synchronized long runJobs() {
+        long minDiff = Long.MAX_VALUE;
+        long now = System.currentTimeMillis();
+        
+        for (int i=0; i < jobs.size();) {
+            JobNode jn = (JobNode) jobs.elementAt(i);
+            if (jn.executeAt.getTime() <= now) {
+                if (tp != null) {
+                    tp.addRequest(jn.job);
+                } else {
+                    Thread jt = new Thread(jn.job);
+                    jt.setDaemon(false);
+                    jt.start();
+                }
+                
+                if (updateJobNode(jn) == null) {
+                    jobs.removeElementAt(i);
+                    dlock.release();
+                }
+            } else {
+                long diff = jn.executeAt.getTime() - now;
+                minDiff = Math.min(diff, minDiff);
+                i++;
+            }
+        }
+        return minDiff;
+    }
+    
+    public synchronized void run() {
+        while (true) {
+            long waitTime = runJobs();
+            try {
+                wait(waitTime);
+            } catch (Exception e) {};
+        }
+    }
+    
+    public void execute(Runnable job) {
+        executeIn(job, (long)0);
+    }
+    
+    public void executeIn(Runnable job, long millis) {
+        executeInAndRepeat(job, millis, 1000, ONCE);
+    }
+    
+    public void executeInAndRepeat(Runnable job, long millis, long repeat) {
+        executeInAndRepeat(job, millis, repeat, FOREVER);
+    }
+    
+    public void executeInAndRepeat(Runnable job, long millis, long repeat, int count) {
+        Date when = new Date(System.currentTimeMillis() + millis);
+        executeAtAndRepeat(job, when, repeat, count);
+    }
+    
+    public void executeAt(Runnable job, Date when) {
+        executeAtAndRepeat(job, when, 1000, ONCE);
+    }
+    
+    public void executeAtAndRepeat(Runnable job, Date when, long repeat) {
+        executeAtAndRepeat(job, when, repeat, FOREVER);
+    }
+    
+    public void executeAtAndRepeat(Runnable job, Date when, long repeat, int count) {
+        JobNode jn = new JobNode();
+        jn.job = job;
+        jn.executeAt = when;
+        jn.interval = repeat;
+        jn.count = count;
+        addJob(jn);
+    }
+    
+    public void cancel(Runnable job) {
+        deleteJob(job);
+    }
 }

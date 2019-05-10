e919921ec8e96b3b27fb72c2bd4151a136f4fbe9
david2999999 <djiang86@binghamton.edu>
2019-05-10 12:36:31 +0000

    usage of read and writer locks

---
+++ Java Threads PDF/Chapter 8 - Advanced Synchronization Topics/Reader Writer Locks/BTree.java
@@ -0,0 +1,9 @@
+public class BTree {
+    public synchronized boolean find(Object o) {
+        // Perform time-consuming search, returning the object if
+        // found or null if the object is not found
+    }
+    public synchronized void insert(Object o) {
+        // Perform a time-consuming insert
+    }
+}

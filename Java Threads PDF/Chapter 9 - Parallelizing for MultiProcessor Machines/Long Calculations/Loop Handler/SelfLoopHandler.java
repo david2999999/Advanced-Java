cda77f5f984469e784933af670ea86917f53edc1
david2999999 <djiang86@binghamton.edu>
2019-05-15 11:57:27 +0000

    self loop handler

---
+++ Java Threads PDF/Chapter 9 - Parallelizing for MultiProcessor Machines/Long Calculations/Loop Handler/SelfLoopHandler.java
@@ -0,0 +1,17 @@
+public class SelfLoopHandler extends LoopHandler {
+    protected int groupSize;
+    
+    public SelfLoopHandler(int start, int end, int size, int threads) {
+        super(start, end, threads);
+        groupSize = size;
+    }
+    protected synchronized LoopRange loopGetRange() {
+        if (curLoop >= endLoop)
+            return null;
+        LoopRange ret = new LoopRange();
+        ret.start = curLoop;
+        curLoop += groupSize;
+        ret.end = (curLoop<endLoop)?curLoop:endLoop;
+        return ret;
+    }
+}

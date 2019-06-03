02fde0f5a9a0477374626b86030bfb759c8fa081
david2999999 <djiang86@binghamton.edu>
2019-06-03 13:38:00 +0000

    thread safe lazy initialization singleton

---
+++ Java Design Patterns - A Programmer's Approach/Singleton/ThreadSafeSingleton.java
@@ -0,0 +1,22 @@
+package com.journaldev.singleton;
+
+public class ThreadSafeSingleton {
+    private static ThreadSafeSingleton instance;
+    private ThreadSafeSingleton(){}
+    
+    public static synchronized ThreadSafeSingleton getInstance(){
+        if(instance == null){
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
+}
+
+// Above implementation works fine and provides thread-safety but it reduces
+// the performance because of cost associated with the synchronized method,
+// although we need it only for the first few threads who might create the
+// separate instances (Read: Java Synchronization). To avoid this extra
+// overhead every time, double checked locking principle is used. In this
+// approach, the synchronized block is used inside if condition with an
+// additional check to ensure that only one instance of singleton class is
+// created

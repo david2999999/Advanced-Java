2e15707092872a15e137ba00bc9f1657acba7f09
david2999999 <djiang86@binghamton.edu>
2019-06-05 11:32:14 +0000

    concrete classes for decorator pattern

---
+++ Java Design Patterns - A Programmer's Approach/Decorator/LuxuryCar.java
@@ -0,0 +1,13 @@
+package com.journaldev.design.decorator;
+
+public class LuxuryCar extends CarDecorator {
+    public LuxuryCar(Car c) {
+        super(c);
+    }
+    
+    @Override
+    public void assemble(){
+        car.assemble();
+        System.out.print(" Adding features of Luxury Car.");
+    }
+}

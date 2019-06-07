aed02322ae0a8b8c145b11f8fa378e0f05d9136e
david2999999 <djiang86@binghamton.edu>
2019-06-07 11:56:54 +0000

    invoker class to encapsulate the command

---
+++ Java Design Patterns - A Programmer's Approach/Behavioral Design Patterns/Command/FileInvoker.java
@@ -0,0 +1,13 @@
+package com.journaldev.design.command;
+
+public class FileInvoker {
+    public Command command;
+    
+    public FileInvoker(Command c){
+        this.command=c;
+    }
+    
+    public void execute(){
+        this.command.execute();
+    }
+}

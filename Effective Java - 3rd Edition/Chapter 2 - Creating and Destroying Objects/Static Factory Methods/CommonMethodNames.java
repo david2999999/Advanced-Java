7a080f21247cdeacd46ce762cea222e71aae8eb9
david2999999 <djiang86@binghamton.edu>
2019-06-25 09:01:12 -0400

    common static factory method names

---
+++ Effective Java - 3rd Edition/Chapter 2 - Creating and Destroying Objects/Static Factory Methods/CommonMethodNames.java
@@ -0,0 +1,10 @@
+public class CommonMethodNames {
+    Date d = Date.from(instant);
+    Set<Rank> faceCards = EnumSet.of(JACK, QUEEN, KING);
+    BigInteger prime = BigInteger.valueOf(Integer.MAX_VALUE);
+    StackWalker luke = StackWalker.getInstance(options);
+    Object newArray = Array.newInstance(classObject, arrayLen);
+    FileStore fs = Files.getFileStore(path);
+    BufferedReader br = Files.newBufferedReader(path);
+    List<Complaint> litany = Collections.list(legacyLitany);
+}

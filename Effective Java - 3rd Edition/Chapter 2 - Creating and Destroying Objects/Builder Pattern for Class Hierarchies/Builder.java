d17bfe7e347dd5cd74afbca9bb3c448adab3076c
david2999999 <djiang86@binghamton.edu>
2019-06-26 07:37:26 -0400

    builder pattern for subclasses

--- Effective Java - 3rd Edition/Chapter 2 - Creating and Destroying Objects/Builder Pattern for Class Hierarchies/Builder.java
+++ Effective Java - 3rd Edition/Chapter 2 - Creating and Destroying Objects/Builder Pattern for Class Hierarchies/Builder.java
@@ -22,4 +22,61 @@ public class Builder {
             toppings = builder.toppings.clone(); // See Item 50
         }
     }
+
+    public class NyPizza extends Pizza {
+        public enum Size {SMALL, MEDIUM, LARGE}
+
+        private final Size size;
+
+        public static class Builder extends Pizza.Builder<Builder> {
+            private final Size size;
+
+            public Builder(Size size) {
+                this.size = Objects.requireNonNull(size);
+            }
+
+            @Override
+            public NyPizza build() {
+                return new NyPizza(this);
+            }
+
+            @Override
+            protected Builder self() {
+                return this;
+            }
+        }
+
+        private NyPizza(Builder builder) {
+            super(builder);
+            size = builder.size;
+        }
+    }
+
+    public class Calzone extends Pizza {
+        private final boolean sauceInside;
+
+        public static class Builder extends Pizza.Builder<Builder> {
+            private boolean sauceInside = false; // Default
+
+            public Builder sauceInside() {
+                sauceInside = true;
+                return this;
+            }
+
+            @Override
+            public Calzone build() {
+                return new Calzone(this);
+            }
+
+            @Override
+            protected Builder self() {
+                return this;
+            }
+        }
+
+        private Calzone(Builder builder) {
+            super(builder);
+            sauceInside = builder.sauceInside;
+        }
+    }
 }

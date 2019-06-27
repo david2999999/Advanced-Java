b72aabf37fc1ea9be36ac5564291e0ea49efeb82
david2999999 <djiang86@binghamton.edu>
2019-06-27 08:10:38 -0400

    try finally examples with multiple streams

---
+++ Effective Java - 3rd Edition/Chapter 2 - Creating and Destroying Objects/Try With Resources/TryFinally.java
@@ -0,0 +1,29 @@
+public class TryFinally {
+    // try-finally - No longer the best way to close resources!
+    static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            return br.readLine();
+        } finally {
+            br.close();
+        }
+
+        // try-finally is ugly when used with more than one resource!
+        static void copy (String src, String dst) throws IOException {
+            InputStream in = new FileInputStream(src);
+            try {
+                OutputStream out = new FileOutputStream(dst);
+                try {
+                    byte[] buf = new byte[BUFFER_SIZE];
+                    int n;
+                    while ((n = in.read(buf)) >= 0)
+                        out.write(buf, 0, n);
+                } finally {
+                    out.close();
+                }
+            } finally {
+                in.close();
+            }
+        }
+    }
+}

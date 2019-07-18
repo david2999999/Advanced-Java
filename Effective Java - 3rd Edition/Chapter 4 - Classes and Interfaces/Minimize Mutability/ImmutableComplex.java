0643256a8c02adec981649b618a292a5880a38ec
david2999999 <djiang86@binghamton.edu>
2019-07-18 07:33:38 -0400

    immutable class with static factories instead of constrcutor

--- Effective Java - 3rd Edition/Chapter 4 - Classes and Interfaces/Minimize Mutability/ImmutableComplex.java
+++ Effective Java - 3rd Edition/Chapter 4 - Classes and Interfaces/Minimize Mutability/ImmutableComplex.java
@@ -2,10 +2,15 @@
 public final class Complex {
     private final double re;
     private final double im;
-    public Complex(double re, double im) {
+
+    private Complex(double re, double im) {
         this.re = re;
         this.im = im;
     }
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
     public double realPart() { return re; }
     public double imaginaryPart() { return im; }
     public Complex plus(Complex c) {

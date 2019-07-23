dc7256fd90d28a469d76d8a1ac1077cc9f79cf22
david2999999 <djiang86@binghamton.edu>
2019-07-23 08:13:58 -0400

    typical use of non static member class

---
+++ Effective Java - 3rd Edition/Chapter 4 - Classes and Interfaces/Favor static member classes over nonstatic/UseOfNonstaticMemberClass.java
@@ -0,0 +1,9 @@
+// Typical use of a nonstatic member class
+public class MySet<E> extends AbstractSet<E> {
+    @Override public Iterator<E> iterator() {
+        return new MyIterator();
+    }
+    private class MyIterator implements Iterator<E> {
+
+    }
+}

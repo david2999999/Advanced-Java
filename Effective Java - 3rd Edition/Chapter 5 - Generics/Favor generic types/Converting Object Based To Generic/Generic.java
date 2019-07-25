89ebcf22d9a2d5b0913b88e3d0d86f3f471a7c44
david2999999 <djiang86@binghamton.edu>
2019-07-25 09:00:45 -0400

    converting object based collection to generic

---
+++ Effective Java - 3rd Edition/Chapter 5 - Generics/Favor generic types/Converting Object Based To Generic/Generic.java
@@ -0,0 +1,29 @@
+// Initial attempt to generify Stack - won't compile!
+public class Stack<E> {
+    private E[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    // The elements array will contain only E instances from push(E).
+    // This is sufficient to ensure type safety, but the runtime
+    // type of the array won't be E[]; it will always be Object[]!
+    @SuppressWarnings("unchecked")
+    public Stack() {
+        elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+    public void push(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    // Appropriate suppression of unchecked warning
+    public E pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+        // push requires elements to be of type E, so cast is correct
+        @SuppressWarnings("unchecked") E result =
+                (E) elements[--size];
+        elements[size] = null; // Eliminate obsolete reference
+        return result;
+    }
+}

091c10dfdc75291d8443c71377bd4c5a9bd17737
david2999999 <djiang86@binghamton.edu>
2019-07-25 08:21:06 -0400

    minimizing the scope of suppress warning

---
+++ Effective Java - 3rd Edition/Chapter 5 - Generics/Eliminate unchecked warnings/MinimizeSuppressWarnings.java
@@ -0,0 +1,25 @@
+public <T> T[] toArray(T[] a) {
+    if (a.length < size)
+        return (T[]) Arrays.copyOf(elements, size, a.getClass());
+
+    System.arraycopy(elements, 0, a, 0, size);
+
+    if (a.length > size)
+        a[size] = null;
+    return a;
+}
+
+// Adding local variable to reduce scope of @SuppressWarnings
+public <T> T[] toArray(T[] a) {
+    if (a.length < size) {
+        // This cast is correct because the array we're creating
+        // is of the same type as the one passed in, which is T[].
+        @SuppressWarnings("unchecked") T[] result = (T[]) Arrays.copyOf(elements, size, a.getClass());
+        return result;
+    }
+    System.arraycopy(elements, 0, a, 0, size);
+    if (a.length > size)
+        a[size] = null;
+
+    return a;
+}

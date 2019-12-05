1c1f1c396cd606ada77e58308d6a9804bd4b11e7
david2999999 <djiang86@binghamton.edu>
2019-12-05 11:10:01 -0500

    inadequate atomicity with atomic thread safe classes

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 2 - Thread Safety/Servlets/UnsafeCachingFactorizer.java
@@ -0,0 +1,20 @@
+@NotThreadSafe
+public class UnsafeCachingFactorizer implements Servlet {
+    private final AtomicReference<BigInteger> lastNumber
+            = new AtomicReference<BigInteger>();
+
+    private final AtomicReference<BigInteger[]> lastFactors
+            = new AtomicReference<BigInteger[]>();
+
+    public void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        if (i.equals(lastNumber.get()))
+            encodeIntoResponse(resp, lastFactors.get() );
+        else {
+            BigInteger[] factors = factor(i);
+            lastNumber.set(i);
+            lastFactors.set(factors);
+            encodeIntoResponse(resp, factors);
+        }
+    }
+}

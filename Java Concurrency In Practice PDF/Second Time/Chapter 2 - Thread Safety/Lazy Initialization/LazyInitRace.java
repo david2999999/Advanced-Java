29a06426f8c75a1e289365d042b20052a862879e
david2999999 <djiang86@binghamton.edu>
2019-12-05 10:23:41 -0500

    race conditions with lazy initialization

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 2 - Thread Safety/Lazy Initialization/LazyInitRace.java
@@ -0,0 +1,10 @@
+@NotThreadSafe
+public class LazyInitRace {
+    private ExpensiveObject instance = null;
+
+    public ExpensiveObject getInstance() {
+        if (instance == null)
+            instance = new ExpensiveObject();
+        return instance;
+    }
+}

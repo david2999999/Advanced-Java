ee7930542a9aec662ace904e3dcda292bb8aa30a
david2999999 <djiang86@binghamton.edu>
2019-12-05 09:34:35 -0500

    race conditions without the necessary synchronization

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 2 - Thread Safety/A Stateless Servlet/UnsafeCountingFactorizer.java
@@ -0,0 +1,13 @@
+@NotThreadSafe
+public class UnsafeCountingFactorizer implements Servlet {
+    private long count = 0;
+
+    public long getCount() { return count; }
+
+    public void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        BigInteger[] factors = factor(i);
+        ++count;
+        encodeIntoResponse(resp, factors);
+    }
+}

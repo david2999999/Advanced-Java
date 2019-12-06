ef3670f41997277fbe37a2cd37e06e3ef787dbdd
david2999999 <djiang86@binghamton.edu>
2019-12-06 08:31:41 -0500

    implicitly allowing the this reference to escape

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 3 - Sharing Objects/Publication and Escape/ThisEscape.java
@@ -0,0 +1,10 @@
+public class ThisEscape {
+    public ThisEscape(EventSource source) {
+        source.registerListener(
+                new EventListener() {
+                    public void onEvent(Event e) {
+                        doSomething(e);
+                    }
+                });
+    }
+}

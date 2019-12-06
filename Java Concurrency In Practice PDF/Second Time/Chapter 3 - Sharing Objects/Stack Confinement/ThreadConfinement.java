3d21ad0c68f480e3f22d21a3487dd3cdd57e84bf
david2999999 <djiang86@binghamton.edu>
2019-12-06 09:18:07 -0500

    thread confinement of local variables

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 3 - Sharing Objects/Stack Confinement/ThreadConfinement.java
@@ -0,0 +1,22 @@
+public class ThreadConfinement {
+    public int loadTheArk(Collection<Animal> candidates) {
+        SortedSet<Animal> animals;
+        int numPairs = 0;
+        Animal candidate = null;
+
+        // animals confined to method, don't let them escape!
+        animals = new TreeSet<Animal>(new SpeciesGenderComparator());
+        animals.addAll(candidates);
+        for (Animal a : animals) {
+            if (candidate == null || !candidate.isPotentialMate(a))
+                candidate = a;
+            else {
+                ark.load(new AnimalPair(candidate, a));
+                ++numPairs;
+                candidate = null;
+            }
+        }
+
+        return numPairs;
+    }
+}

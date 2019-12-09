f6564a6a98e9ccc9a66f369249cf2cb61dd6e431
david2999999 <djiang86@binghamton.edu>
2019-12-09 08:42:24 -0500

    immutable class built out of mutable underlying objects

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 3 - Sharing Objects/Immutability/ThreeStooges.java
@@ -0,0 +1,14 @@
+@Immutable
+public final class ThreeStooges {
+    private final Set<String> stooges = new HashSet<String>();
+
+    public ThreeStooges() {
+        stooges.add("Moe");
+        stooges.add("Larry");
+        stooges.add("Curly");
+    }
+
+    public boolean isStooge(String name) {
+        return stooges.contains(name);
+    }
+}

95c8495026f094bb9f0906fa1d31907b484dd1a6
david2999999 <djiang86@binghamton.edu>
2019-12-13 08:07:23 -0500

    delegating thread safety

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 4 - Composing Objects/Delegating Thread Safety/MutablePoint.java
@@ -0,0 +1,10 @@
+@NotThreadSafe
+public class MutablePoint {
+    public int x, y;
+    public MutablePoint() { x = 0; y = 0; }
+
+    public MutablePoint(MutablePoint p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+}

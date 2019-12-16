7db200fc020c03e8dcd06be14b078536638e9a12
david2999999 <djiang86@binghamton.edu>
2019-12-16 08:34:29 -0500

    non-thread safe implementation of put if absent

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 4 - Composing Objects/Client Side Locking/ListHelper.java
@@ -0,0 +1,12 @@
+@NotThreadSafe
+public class ListHelper<E> {
+    public List<E> list =
+            Collections.synchronizedList(new ArrayList<E>());
+
+    public synchronized boolean putIfAbsent(E x) {
+        boolean absent = !list.contains(x);
+        if (absent)
+            list.add(x);
+        return absent;
+    }
+}

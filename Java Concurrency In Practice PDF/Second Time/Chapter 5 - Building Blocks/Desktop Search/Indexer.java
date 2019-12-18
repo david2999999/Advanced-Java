7ed5aba9b89ec95ad841f18d071f841e881366a7
david2999999 <djiang86@binghamton.edu>
2019-12-18 10:13:35 -0500

    file indexer thread with blocking queue

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 5 - Building Blocks/Desktop Search/Indexer.java
@@ -0,0 +1,16 @@
+public class Indexer implements Runnable {
+    private final BlockingQueue<File> queue;
+
+    public Indexer(BlockingQueue<File> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            while (true)
+                indexFile(queue.take());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}

ba033c733f65adb5a3dd7e0bb1c6a592bb2f18da
david2999999 <djiang86@binghamton.edu>
2019-12-18 10:14:17 -0500

    desktop search with blocking queue and 2 threads

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 5 - Building Blocks/Desktop Search/DesktopSearch.java
@@ -0,0 +1,14 @@
+public class DesktopSearch {
+    public static void startIndexing(File[] roots) {
+        BlockingQueue<File> queue = new LinkedBlockingQueue<File>(BOUND);
+        FileFilter filter = new FileFilter() {
+            public boolean accept(File file) { return true; }
+        };
+
+        for (File root : roots)
+            new Thread(new FileCrawler(queue, filter, root)).start();
+
+        for (int i = 0; i < N_CONSUMERS; i++)
+            new Thread(new Indexer(queue)).start();
+    }
+}

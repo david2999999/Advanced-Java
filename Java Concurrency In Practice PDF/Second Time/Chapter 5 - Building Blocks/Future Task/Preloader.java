4067cf05ac62944cb8de2713d1f0d2537f1b2787
david2999999 <djiang86@binghamton.edu>
2019-12-20 09:19:16 -0500

    coercing an unchecked throwable to a runtime exception

--- Java Concurrency In Practice PDF/Second Time/Chapter 5 - Building Blocks/Future Task/Preloader.java
+++ Java Concurrency In Practice PDF/Second Time/Chapter 5 - Building Blocks/Future Task/Preloader.java
@@ -22,4 +22,16 @@ public class Preloader {
                 throw launderThrowable(cause);
         }
     }
+
+    /** If the Throwable is an Error, throw it; if it is a
+     * RuntimeException return it, otherwise throw IllegalStateException
+     */
+    public static RuntimeException launderThrowable(Throwable t) {
+        if (t instanceof RuntimeException)
+            return (RuntimeException) t;
+        else if (t instanceof Error)
+            throw (Error) t;
+        else
+            throw new IllegalStateException("Not unchecked", t);
+    }
 }

40e14f510aa6ac9b072af80a2f5dda64f2596b6c
david2999999 <djiang86@binghamton.edu>
2020-01-06 08:32:18 -0500

    single thread web server

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 6 - Task Execution/Executing Tasks Sequentially/SingleThreadWebServer.java
@@ -0,0 +1,9 @@
+class SingleThreadWebServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        while (true) {
+            Socket connection = socket.accept();
+            handleRequest(connection);
+        }
+    }
+}

5eb714dd6d18d1c7e32c92b96e5108a4e208aebe
david2999999 <djiang86@binghamton.edu>
2020-01-07 08:38:28 -0500

    web server using a thread pool

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 6 - Task Execution/The Executor Framework/TaskExecutionWebServer.java
@@ -0,0 +1,18 @@
+class TaskExecutionWebServer {
+    private static final int NTHREADS = 100;
+    private static final Executor exec
+            = Executors.newFixedThreadPool(NTHREADS);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        while (true) {
+            final Socket connection = socket.accept();
+            Runnable task = new Runnable() {
+                public void run() {
+                    handleRequest(connection);
+                }
+            };
+            exec.execute(task);
+        }
+    }
+}

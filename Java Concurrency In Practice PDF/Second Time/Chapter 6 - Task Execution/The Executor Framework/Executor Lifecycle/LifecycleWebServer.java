2725879f3845b8fbe7bd6d7a8d60b6000e611938
david2999999 <djiang86@binghamton.edu>
2020-01-08 08:44:51 -0500

    web server with shutdown support

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 6 - Task Execution/The Executor Framework/Executor Lifecycle/LifecycleWebServer.java
@@ -0,0 +1,28 @@
+class LifecycleWebServer {
+    private final ExecutorService exec = ...;
+
+    public void start() throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        while (!exec.isShutdown()) {
+            try {
+                final Socket conn = socket.accept();
+                exec.execute(new Runnable() {
+                    public void run() { handleRequest(conn); }
+                });
+            } catch (RejectedExecutionException e) {
+                if (!exec.isShutdown())
+                    log("task submission rejected", e);
+            }
+        }
+    }
+
+    public void stop() { exec.shutdown(); }
+
+    void handleRequest(Socket connection) {
+        Request req = readRequest(connection);
+        if (isShutdownRequest(req))
+            stop();
+        else
+            dispatchRequest(req);
+    }
+}

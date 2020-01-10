c7f05646a8cbc4ba1b2b5917638702648cb0c7ed
david2999999 <djiang86@binghamton.edu>
2020-01-10 09:02:03 -0500

    using volatile field to hold cancellation state

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Volatile Field for Cancellation State/PrimeGenerator.java
@@ -0,0 +1,23 @@
+@ThreadSafe
+public class PrimeGenerator implements Runnable {
+    @GuardedBy("this")
+    private final List<BigInteger> primes
+            = new ArrayList<BigInteger>();
+    private volatile boolean cancelled;
+
+    public void run() {
+        BigInteger p = BigInteger.ONE;
+        while (!cancelled ) {
+            p = p.nextProbablePrime();
+            synchronized (this) {
+                primes.add(p);
+            }
+        }
+    }
+
+    public void cancel() { cancelled = true; }
+
+    public synchronized List<BigInteger> get() {
+        return new ArrayList<BigInteger>(primes);
+    }
+}
---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Volatile Field for Cancellation State/PrimeNumber.java
@@ -0,0 +1,12 @@
+public class PrimeNumber {
+    List<BigInteger> aSecondOfPrimes() throws InterruptedException {
+        PrimeGenerator generator = new PrimeGenerator();
+        new Thread(generator).start();
+        try {
+            SECONDS.sleep(1);
+        } finally {
+            generator.cancel();
+        }
+        return generator.get();
+    }
+}

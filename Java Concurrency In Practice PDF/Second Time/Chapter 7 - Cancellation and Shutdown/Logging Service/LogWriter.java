712e0650130ca24566524cd4ebf33db7f235a664
david2999999 <djiang86@binghamton.edu>
2020-01-13 09:07:27 -0500

    producer consumer logging service with no shutdown support

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Logging Service/LogWriter.java
@@ -0,0 +1,29 @@
+public class LogWriter {
+    private final BlockingQueue<String> queue;
+    private final LoggerThread logger;
+
+    public LogWriter(Writer writer) {
+        this.queue = new LinkedBlockingQueue<String>(CAPACITY);
+        this.logger = new LoggerThread(writer);
+    }
+
+    public void start() { logger.start(); }
+
+    public void log(String msg) throws InterruptedException {
+        queue.put(msg);
+    }
+
+    private class LoggerThread extends Thread {
+        private final PrintWriter writer;
+
+        public void run() {
+            try {
+                while (true)
+                    writer.println(queue.take());
+            } catch(InterruptedException ignored) {
+            } finally {
+                writer.close();
+            }
+        }
+    }
+}

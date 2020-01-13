096f0a729bbfe237318fb34eaece432eb13697ed
david2999999 <djiang86@binghamton.edu>
2020-01-13 08:11:49 -0500

    do not schedule an interrupt on a borrowed thread

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Interruption/BorrowedThread.java
@@ -0,0 +1,12 @@
+public class BorrowedThread {
+    private static final ScheduledExecutorService cancelExec = ...;
+
+    public static void timedRun(Runnable r,
+                                long timeout, TimeUnit unit) {
+        final Thread taskThread = Thread.currentThread();
+        cancelExec.schedule(new Runnable() {
+            public void run() { taskThread.interrupt(); }
+        }, timeout, unit);
+        r.run();
+    }
+}

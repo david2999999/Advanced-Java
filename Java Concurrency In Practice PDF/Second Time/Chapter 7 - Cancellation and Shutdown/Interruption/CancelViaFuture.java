32e3670ed88337ebfc1a261849ba09619b948771
david2999999 <djiang86@binghamton.edu>
2020-01-13 08:19:47 -0500

    cancelling a task using future

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Interruption/CancelViaFuture.java
@@ -0,0 +1,18 @@
+public class CancelViaFuture {
+    public static void timedRun(Runnable r,
+                                long timeout, TimeUnit unit)
+            throws InterruptedException {
+        Future<?> task = taskExec.submit(r);
+        try {
+            task.get(timeout, unit);
+        } catch (TimeoutException e) {
+            // task will be cancelled below
+        } catch (ExecutionException e) {
+            // exception thrown in task; rethrow
+            throw launderThrowable(e.getCause());
+        } finally {
+            // Harmless if task already completed
+            task.cancel(true); // interrupt if running
+        }
+    }
+}

d851e4ab2020dd3587ac73385a06d0394b124ffb
david2999999 <djiang86@binghamton.edu>
2020-01-14 08:14:31 -0500

    log service using executor service

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Executor Service/LogService.java
@@ -0,0 +1,19 @@
+public class LogService {
+    private final ExecutorService exec = newSingleThreadExecutor();
+
+    public void start() { }
+    public void stop() throws InterruptedException {
+        try {
+            exec.shutdown();
+            exec.awaitTermination(TIMEOUT, UNIT);
+        } finally {
+            writer.close();
+        }
+    }
+
+    public void log(String msg) {
+        try {
+            exec.execute(new WriteTask(msg));
+        } catch (RejectedExecutionException ignored) { }
+    }
+}

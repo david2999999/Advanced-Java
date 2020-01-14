b1a162f49626833e74248f0e74c21eed9c730a18
david2999999 <djiang86@binghamton.edu>
2020-01-14 09:02:06 -0500

    using a private executor whose lifetime is bounded by a mthod call

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Executor/MailService.java
@@ -0,0 +1,21 @@
+public class MailService {
+    boolean checkMail(Set<String> hosts, long timeout, TimeUnit unit)
+            throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final AtomicBoolean hasNewMail = new AtomicBoolean(false);
+
+        try {
+            for (final String host : hosts)
+                exec.execute(new Runnable() {
+                    public void run() {
+                        if (checkMail(host))
+                            hasNewMail.set(true);
+                    }
+                });
+        } finally {
+            exec.shutdown();
+            exec.awaitTermination(timeout, unit);
+        }
+        return hasNewMail.get();
+    }
+}

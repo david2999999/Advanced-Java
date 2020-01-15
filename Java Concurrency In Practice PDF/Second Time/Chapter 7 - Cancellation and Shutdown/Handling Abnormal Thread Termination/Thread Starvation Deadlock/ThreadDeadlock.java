fdec08bae20d54552853c4e555ccb91ca6cf49ee
david2999999 <djiang86@binghamton.edu>
2020-01-15 09:38:06 -0500

    thread deadlock in a single threaded executor

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Handling Abnormal Thread Termination/Thread Starvation Deadlock/ThreadDeadlock.java
@@ -0,0 +1,14 @@
+public class ThreadDeadlock {
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    public class RenderPageTask implements Callable<String> {
+        public String call() throws Exception {
+            Future<String> header, footer;
+            header = exec.submit(new LoadFileTask("header.html"));
+            footer = exec.submit(new LoadFileTask("footer.html"));
+            String page = renderBody();
+            // Will deadlock -- task waiting for result of subtask
+            return header.get() + page + footer.get();
+        }
+    }
+}

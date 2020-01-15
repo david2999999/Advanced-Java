bb3681c43b993ca7a5a3cf699d0d058b14f917e0
david2999999 <djiang86@binghamton.edu>
2020-01-15 08:18:44 -0500

    web crawler for with tracking executor

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Limitation of Shutdownnow/WebCrawler.java
@@ -0,0 +1,46 @@
+public abstract class WebCrawler {
+    private volatile TrackingExecutor exec;
+    @GuardedBy("this")
+    private final Set<URL> urlsToCrawl = new HashSet<URL>();
+
+    public synchronized void start() {
+        exec = new TrackingExecutor(
+                Executors.newCachedThreadPool());
+        for (URL url : urlsToCrawl) submitCrawlTask(url);
+        urlsToCrawl.clear();
+    }
+
+    public synchronized void stop() throws InterruptedException {
+        try {
+            saveUncrawled(exec.shutdownNow());
+            if (exec.awaitTermination(TIMEOUT, UNIT))
+                saveUncrawled(exec.getCancelledTasks());
+        } finally {
+            exec = null;
+        }
+    }
+
+    protected abstract List<URL> processPage(URL url);
+
+    private void saveUncrawled(List<Runnable> uncrawled) {
+        for (Runnable task : uncrawled)
+            urlsToCrawl.add(((CrawlTask) task).getPage());
+    }
+
+    private void submitCrawlTask(URL u) {
+        exec.execute(new CrawlTask(u));
+    }
+
+    private class CrawlTask implements Runnable {
+        private final URL url;
+
+        public void run() {
+            for (URL link : processPage(url)) {
+                if (Thread.currentThread().isInterrupted())
+                    return;
+                submitCrawlTask(link);
+            }
+        }
+        public URL getPage() { return url; }
+    }
+}

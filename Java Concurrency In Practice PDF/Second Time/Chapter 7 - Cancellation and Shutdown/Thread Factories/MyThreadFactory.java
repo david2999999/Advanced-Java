2bc4b7d52b3b907823de750d48ff5a8008ca708a
david2999999 <djiang86@binghamton.edu>
2020-01-16 08:58:36 -0500

    custom thread factory for thread generation

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 7 - Cancellation and Shutdown/Thread Factories/MyThreadFactory.java
@@ -0,0 +1,11 @@
+public class MyThreadFactory implements ThreadFactory {
+    private final String poolName;
+
+    public MyThreadFactory(String poolName) {
+        this.poolName = poolName;
+    }
+
+    public Thread newThread(Runnable runnable) {
+        return new MyAppThread(runnable, poolName);
+    }
+}

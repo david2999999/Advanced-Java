24d6204b385618da728f5ec7909f0992223b0dc9
david2999999 <djiang86@binghamton.edu>
2020-01-20 08:40:58 -0500

    transforming sequential execution into parallel execution

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 8 - Applying Thread Pools/Parallelizing Recursive Algorithms/ParallelExecution.java
@@ -0,0 +1,13 @@
+public class ParallelExecution {
+    void processSequentially(List<Element> elements) {
+        for (Element e : elements)
+            process(e);
+    }
+
+    void processInParallel(Executor exec, List<Element> elements) {
+        for (final Element e : elements)
+            exec.execute(new Runnable() {
+                public void run() { process(e); }
+            });
+    }
+}

e3591708000973bd19ccbb4df38d816389a2c5ab
david2999999 <djiang86@binghamton.edu>
2020-01-20 08:54:46 -0500

    trasforming sequential tail recursion into parallelized recursion

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 8 - Applying Thread Pools/Parallelizing Recursive Algorithms/ParallelRecursion.java
@@ -0,0 +1,22 @@
+public class ParallelRecursion {
+    public<T> void sequentialRecursive(List<Node<T>> nodes,
+                                       Collection<T> results) {
+        for (Node<T> n : nodes) {
+            results.add(n.compute());
+            sequentialRecursive(n.getChildren(), results);
+        }
+    }
+
+    public<T> void parallelRecursive(final Executor exec,
+                                     List<Node<T>> nodes,
+                                     final Collection<T> results) {
+        for (final Node<T> n : nodes) {
+            exec.execute(new Runnable() {
+                public void run() {
+                    results.add(n.compute());
+                }
+            });
+            parallelRecursive(exec, n.getChildren(), results);
+        }
+    }
+}

ade093cb1da6bc8767b97863a8fc9f5b2398c1b9
david2999999 <djiang86@binghamton.edu>
2020-01-22 08:54:15 -0500

    puzzle solver that recognizes when no solutions exist

---
+++ Java Concurrency In Practice PDF/Second Time/Chapter 8 - Applying Thread Pools/Puzzle Framework/Concurrent/PuzzleSolver.java
@@ -0,0 +1,22 @@
+public class PuzzleSolver<P,M> extends ConcurrentPuzzleSolver<P,M> {
+    private final AtomicInteger taskCount = new AtomicInteger(0);
+
+    protected Runnable newTask(P p, M m, Node<P,M> n) {
+        return new CountingSolverTask(p, m, n);
+    }
+
+    class CountingSolverTask extends SolverTask {
+        CountingSolverTask(P pos, M move, Node<P, M> prev) {
+            super(pos, move, prev);
+            taskCount.incrementAndGet();
+        }
+        public void run() {
+            try {
+                super.run();
+            } finally {
+                if (taskCount.decrementAndGet() == 0)
+                    solution.setValue(null);
+            }
+        }
+    }
+}

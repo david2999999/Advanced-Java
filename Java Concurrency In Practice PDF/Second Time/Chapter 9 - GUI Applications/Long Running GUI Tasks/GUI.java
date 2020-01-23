39aa1d302829d48e90d5944a1d55a74dc90bf488
david2999999 <djiang86@binghamton.edu>
2020-01-23 08:32:28 -0500

    long running task with user feedback

--- Java Concurrency In Practice PDF/Second Time/Chapter 9 - GUI Applications/Long Running GUI Tasks/GUI.java
+++ Java Concurrency In Practice PDF/Second Time/Chapter 9 - GUI Applications/Long Running GUI Tasks/GUI.java
@@ -8,5 +8,26 @@ public class GUI {
                     public void run() { doBigComputation(); }
                 });
             }});
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                button.setEnabled(false);
+                label.setText("busy");
+                backgroundExec.execute(new Runnable() {
+                    public void run() {
+                        try {
+                            doBigComputation();
+                        } finally {
+                            GuiExecutor.instance().execute(new Runnable() {
+                                public void run() {
+                                    button.setEnabled(true);
+                                    label.setText("idle");
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 }

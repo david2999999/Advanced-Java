746670ab94c2fbb3c53ae54f850d982d48e9a4b2
david2999999 <djiang86@binghamton.edu>
2020-01-23 08:40:46 -0500

    cancelling a long running task

--- Java Concurrency In Practice PDF/Second Time/Chapter 9 - GUI Applications/Long Running GUI Tasks/GUI.java
+++ Java Concurrency In Practice PDF/Second Time/Chapter 9 - GUI Applications/Long Running GUI Tasks/GUI.java
@@ -29,5 +29,30 @@ public class GUI {
                 });
             }
         });
+
+        Future<?> runningTask = null; // thread-confined
+
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (runningTask != null) {
+                    runningTask = backgroundExec.submit(new Runnable() {
+                        public void run() {
+                            while (moreWork()) {
+                                if (Thread.currentThread().isInterrupted()) {
+                                    cleanUpPartialWork();
+                                    break;
+                                }
+                                doSomeWork();
+                            }
+                        }
+                    });
+                };
+            }});
+
+        cancelButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                if (runningTask != null)
+                    runningTask.cancel(true);
+            }});
     }
 }

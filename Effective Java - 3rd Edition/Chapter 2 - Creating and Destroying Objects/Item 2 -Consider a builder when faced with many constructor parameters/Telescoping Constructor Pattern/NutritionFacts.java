993e52d12e48e589edeb7562ecff0975e27cbb69
david2999999 <djiang86@binghamton.edu>
2020-02-07 15:15:10 -0500

    telescoping constructor pattern does not scale well

---
+++ Effective Java - 3rd Edition/Chapter 2 - Creating and Destroying Objects/Item 2 -Consider a builder when faced with many constructor parameters/Telescoping Constructor Pattern/NutritionFacts.java
@@ -0,0 +1,44 @@
+// Telescoping constructor pattern - does not scale well!
+public class NutritionFacts {
+    private final int servingSize; // (mL) required
+    private final int servings; // (per container) required
+    private final int calories; // (per serving) optional
+    private final int fat; // (g/serving) optional
+    private final int sodium; // (mg/serving) optional
+    private final int carbohydrate; // (g/serving) optional
+
+    public NutritionFacts(int servingSize, int servings) {
+        this(servingSize, servings, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories) {
+        this(servingSize, servings, calories, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat) {
+        this(servingSize, servings, calories, fat, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat, int sodium) {
+        this(servingSize, servings, calories, fat, sodium, 0);
+    }
+
+    public NutritionFacts(int servingSize, int servings,
+                          int calories, int fat, int sodium, int carbohydrate) {
+        this.servingSize = servingSize;
+        this.servings = servings;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
+}
+
+// NutritionFacts cocaCola =
+//new NutritionFacts(240, 8, 100, 0, 35, 27);
+
+// In short, the telescoping constructor pattern works, but it is hard to write
+//client code when there are many parameters, and harder still to read it.

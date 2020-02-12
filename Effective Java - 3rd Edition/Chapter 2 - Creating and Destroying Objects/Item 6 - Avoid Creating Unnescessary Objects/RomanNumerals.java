fc85e88a558afde66df6aaf958960dce226273f7
david2999999 <djiang86@binghamton.edu>
2020-02-12 13:22:23 -0500

    reusing expensive objects for improved performance

---
+++ Effective Java - 3rd Edition/Chapter 2 - Creating and Destroying Objects/Item 6 - Avoid Creating Unnescessary Objects/RomanNumerals.java
@@ -0,0 +1,10 @@
+// Reusing expensive object for improved performance
+public class RomanNumerals {
+    private static final Pattern ROMAN = Pattern.compile(
+            "^(?=.)M*(C[MD]|D?C{0,3})"
+                    + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    static boolean isRomanNumeral(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+}

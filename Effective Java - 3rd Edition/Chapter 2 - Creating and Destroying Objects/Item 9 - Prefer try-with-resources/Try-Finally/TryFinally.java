f87e43f22731fdd3993d3b76a99d31a0f62d728a
david2999999 <djiang86@binghamton.edu>
2020-02-12 15:18:46 -0500

    do not use try finally to close resources

---
+++ Effective Java - 3rd Edition/Chapter 2 - Creating and Destroying Objects/Item 9 - Prefer try-with-resources/Try-Finally/TryFinally.java
@@ -0,0 +1,28 @@
+public class TryFinally {
+    public static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            return br.readLine();
+        } finally {
+            br.close();
+        }
+    }
+
+    // try-finally is ugly when used with more than one resource!
+    public static void copy(String src, String dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                byte[] buf = new byte[BUFFER_SIZE];
+                int n;
+                while ((n = in.read(buf)) >= 0)
+                    out.write(buf, 0, n);
+            } finally {
+                out.close();
+            }}
+        finally {
+            in.close();
+        }
+    }
+}

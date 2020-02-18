7b2271ba7fb0ce5189a6b1cd383f706c32531995
david2999999 <djiang86@binghamton.edu>
2020-02-18 09:04:34 -0500

    subject interface in Observer design pattern

---
+++ Java Design Pattern - Reusable Solutions to Common Problems/Chapter 7 - Observer Design Pattern/Interface/Subject.java
@@ -0,0 +1,6 @@
+public interface Subject {
+    public void subscribeObserver(Observer observer);
+    public void unSubscribeObserver(Observer observer);
+    public void notifyObservers();
+    public String subjectDetails();
+}

99dc3fe6c26a7dd304fec7743d1cc89b646fdb9a
david2999999 <djiang86@binghamton.edu>
2020-02-20 09:37:10 -0500

    multiple heavy classes to be used with fly weight pattern

---
+++ Java Design Pattern - Reusable Solutions to Common Problems/Chapter 11 - Flyweight Pattern/Platform/RubyPlatform.java
@@ -0,0 +1,10 @@
+public class RubyPlatform implements Platform {
+    public RubyPlatform() {
+        System.out.println("RubyPlatform object created");
+    }
+
+    @Override
+    public void execute(Code code) {
+        System.out.println("Compiling and executing Ruby code.");
+    }
+}

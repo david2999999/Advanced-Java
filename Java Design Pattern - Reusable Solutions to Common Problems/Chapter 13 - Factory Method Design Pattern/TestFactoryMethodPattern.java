407fb9220182b76346b87b08224a01cbd55938cc
david2999999 <djiang86@binghamton.edu>
2020-02-27 08:53:15 -0500

    testing factory method pattern

---
+++ Java Design Pattern - Reusable Solutions to Common Problems/Chapter 13 - Factory Method Design Pattern/TestFactoryMethodPattern.java
@@ -0,0 +1,15 @@
+public class TestFactoryMethodPattern {
+    public static void main(String[] args) {
+        DisplayService service = new FeedbackXMLDisplayService();
+        service.display();
+
+        service = new ErrorXMLDisplayService();
+        service.display();
+
+        service = new OrderXMLDisplayService();
+        service.display();
+
+        service = new ResponseXMLDisplayService();
+        service.display();
+    }
+}

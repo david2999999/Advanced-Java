4a4c76b392f60625fba489888afd74c82607ea23
david2999999 <djiang86@binghamton.edu>
2020-02-28 08:11:00 -0500

    prototype design pattern - when instances of a class can have a few combinations of state, it may be more convenient to install the corresponding number of prototypes and clone them rather than instantiating the class manually

---
+++ Java Design Pattern - Reusable Solutions to Common Problems/Chapter 15 - Prototype Design Pattern/Access Controls/AccessControlProvider.java
@@ -0,0 +1,20 @@
+public class AccessControlProvider {
+    private static Map<String, AccessControl> map = new HashMap<String, AccessControl>();
+
+    static {
+        System.out.println("Fetching data from external resources and creating access control objects...");
+        map.put("USER", new AccessControl("USER", "DO_WORK"));
+        map.put("ADMIN", new AccessControl("ADMIN", "ADD/REMOVE USERS"));
+        map.put("MANAGER", new AccessControl("MANAGER", "GENERATE/READ REPORTS"));
+        map.put("VP", new AccessControl("VP", "MODIFY REPORTS"));
+    }
+
+    public static AccessControl getAccessControlObject(String controlLevel) {
+        AccessControl ac = null;
+        ac = map.get(controlLevel);
+        if (ac != null) {
+            return ac.clone();
+        }
+        return null;
+    }
+}

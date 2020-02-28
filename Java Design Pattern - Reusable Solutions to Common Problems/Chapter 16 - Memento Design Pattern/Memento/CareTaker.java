8a622c92dc59a00e4dad8b562756bc7b1023e1b4
david2999999 <djiang86@binghamton.edu>
2020-02-28 09:17:42 -0500

    memento and care taker to keep track of previous states of our originator object class

---
+++ Java Design Pattern - Reusable Solutions to Common Problems/Chapter 16 - Memento Design Pattern/Memento/CareTaker.java
@@ -0,0 +1,18 @@
+public class CareTaker {
+    private final Map<String, Memento> savepointStorage = new HashMap<String, Memento>();
+
+    public void saveMemento(Memento memento, String savepointName) {
+        System.out.println("Saving state..." + savepointName);
+        savepointStorage.put(savepointName, memento);
+    }
+
+    public Memento getMemento(String savepointName) {
+        System.out.println("Undo at ..." + savepointName);
+        return savepointStorage.get(savepointName);
+    }
+
+    public void clearSavepoints() {
+        System.out.println("Clearing all save points...");
+        savepointStorage.clear();
+    }
+}
